5f59668051f42c1e0258e441dec94654bb9eeb06
Son DoHyun <1son2son@o.cnu.ac.kr>
2025-09-20 13:28:14 +0900

    33
    33

--- BlockViz.Application/obj/Debug/net8.0-windows/BlockViz.Applications.AssemblyInfo.cs
+++ BlockViz.Application/obj/Debug/net8.0-windows/BlockViz.Applications.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockViz.Applications")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44426a14143e0272398132b26de4d9c3e71e0ee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afabcc63ea14b23548b55b7560caf1f280313da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockViz.Applications")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockViz.Applications")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
